05f28203fe08a81afa9251d97e20fac258cb6b00
Émilien André <emilien.andre@idakto.com>
2024-09-18 16:03:37 +0100

    marche par défaut ?

--- gamemode/player_class/player.lua
+++ gamemode/player_class/player.lua
@@ -5,7 +5,7 @@ local PLAYER = {}
 
 PLAYER.DisplayName			= "Player"
 
-PLAYER.WalkSpeed			= 200		-- How fast to move when not running
+PLAYER.WalkSpeed			= 100		-- How fast to move when not running
 PLAYER.RunSpeed				= 600		-- How fast to move when running
 PLAYER.CrouchedWalkSpeed	= 0.3		-- Multiply move speed by this when crouching
 PLAYER.DuckSpeed			= 0.3		-- How fast to go from not ducking, to ducking
